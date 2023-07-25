e77cc1ff744534c36ff40c2d9e34aa864834b1fd
dajungkim-cuboxai <dajung.kim@cubox.ai>
2023-07-25 17:31:12 +0900

    jsconfig.json 경로 이동

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import styles from './styles/App.module.css';
-import leftMeme from './img/left_meme.png';
-import rightMeme from './img/right_meme.png';
+import styles from 'styles/App.module.css';
+import leftMeme from 'img/left_meme.png';
+import rightMeme from 'img/right_meme.png';
 import { useState } from "react";
 
 
@@ -8,24 +8,28 @@ export default function App() {
 
     const [left, setLeft] = useState(true);
 
-    const inMouse = (e) => {
+    const inMouse = () => {
         setLeft(false);
     }
 
-    const outMouse = (e) => {
+    const outMouse = () => {
         setLeft(true);
     }
 
     return (
         <div>
-            <img src={
-                `${left ? leftMeme : rightMeme}`
-            } alt={"MEME"}
-                 onMouseEnter={inMouse}
-                 onMouseOut={outMouse}
+            <img
+                alt={"고양이"}
 
-                 onTouchStart={inMouse}
-                 onTouchEnd={outMouse}
+                src={
+                    `${left ? leftMeme : rightMeme}`
+                }
+
+                onMouseEnter={inMouse}
+                onMouseOut={outMouse}
+
+                onTouchStart={inMouse}
+                onTouchEnd={outMouse}
             />
         </div>
     );
