8197518c47b1251e88b269359dab535e69c435e6
dajungkim-cuboxai <dajung.kim@cubox.ai>
2023-07-26 09:14:58 +0900

    페이지 공유 추가

--- src/App.js
+++ src/App.js
@@ -16,16 +16,36 @@ export default function App() {
         setLeft(true);
     }
 
-    const copyText = async (text) => {
-
+    const copyText = async (data) => {
         try {
-
-            await navigator.clipboard.writeText(text);
-            console.log(text)
+            // await navigator.clipboard.writeText(data);
+            await navigator.clipboard;
 
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const onShare = () => {
+        const shareObj = {
+          title: "고양이 귀여워",
+          text: "귀여운 고양이에 마우스를 올리거나 터치해보세요.",
+          url: window.location.href
+        };
+
+        if( navigator.share ) {
+            navigator
+                .share(shareObj)
+                .then(() => {
+                    alert("공유 성공");
+                })
+                .catch((error) => {
+                    alert("공유 실패");
+                });
+        }else {
+            alert("페이지 공유 지원 불가");
+        }
+
     }
 
     return (
@@ -43,7 +63,8 @@ export default function App() {
                 onTouchStart={inMouse}
                 onTouchEnd={outMouse}
             />
-            <button onClick={() => copyText(`${window.location.href}`)}>공유하기</button>
+            {/*<button onClick={() => copyText(`${window.location.href}`)}>공유하기</button>*/}
+            <button onClick={onShare}>공유하기</button>
         </div>
     );
 };
